31fd38197543a0f817602ccd984edd88defeb3a3
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2023-10-13 16:34:48 +0800

    feat: Init element-plus, user avatar.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-
+import { User } from "@element-plus/icons-vue"
 </script>
 
 <template>
-  <div class="w-full h-full">
-    <div class="w-1/5 h-full"> 114514 </div>
-    <div class="w-4/5 h-full"> 1919810 </div>
+  <div class="flex justify-center h-[100vh]">
+    <div class="basis-1/5 px-10 py-5">
+      <div class="h-10 w-full">
+        <el-avatar :icon="User" />
+      </div>
+      <div class="flex flex-col">
+        <div class=""></div>
+        <el-divider />
+        <div class=""></div>
+        <div class="before:grow "></div>
+      </div>
+    </div>
+    <div class="basis-4/5 bg-background-100"></div>
   </div>
 </template>
 
