65532f9e50e7a33e9a1bffeab03d269f898fb1c3
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2023-12-12 01:50:05 +0800

    Update dependencies and refactor Devices.vue

--- frontend/src/routes/Devices.vue
+++ frontend/src/routes/Devices.vue
@@ -1,16 +1,9 @@
 <template>
   <div class="flex flex-wrap gap-3">
     <DeviceStatusCard
-      v-for="device in devices"
-      :roomId="device.roomId"
-      :isOn="device.isOn"
-      :temperature="device.temperature"
-      :mode="device.mode"
-      :windSpeed="device.windSpeed"
-      :isWeeping="device.isWeeping"
-      :lastUpdate="device.lastUpdate"
-      @update-wind-speed="(n: number) => (device.windSpeed = n)"
-      @update-is-on="() => console.log(device.isOn)"
+      v-for="(roomId, index) in roomIDs"
+      :roomId="roomId"
+      :key="index"
       class="alid min-w-[calc(33%-0.375rem)] shrink-0"
     />
   </div>
@@ -19,77 +12,7 @@
 <script lang="ts" setup>
 import DeviceStatusCard from "../components/DeviceStatusCard.vue";
 
-enum mode {
-  cold = "制冷",
-  hot = "制热",
-  auto = "自动"
-}
-
-const devices = ref([
-  {
-    roomId: "114",
-    isOn: true,
-    temperature: 25,
-    mode: mode.cold,
-    windSpeed: 2,
-    isWeeping: false,
-    lastUpdate: new Date()
-  },
-  {
-    roomId: "514",
-    isOn: false,
-    temperature: 25,
-    mode: mode.hot,
-    windSpeed: 5,
-    isWeeping: false,
-    lastUpdate: new Date()
-  },
-  {
-    roomId: "191",
-    isOn: true,
-    temperature: 25,
-    mode: mode.auto,
-    windSpeed: 8,
-    isWeeping: false,
-    lastUpdate: new Date()
-  },
-  {
-    roomId: "114",
-    isOn: true,
-    temperature: 25,
-    mode: mode.cold,
-    windSpeed: 2,
-    isWeeping: false,
-    lastUpdate: new Date()
-  },
-  {
-    roomId: "514",
-    isOn: false,
-    temperature: 25,
-    mode: mode.hot,
-    windSpeed: 5,
-    isWeeping: false,
-    lastUpdate: new Date()
-  },
-  {
-    roomId: "191",
-    isOn: true,
-    temperature: 25,
-    mode: mode.auto,
-    windSpeed: 8,
-    isWeeping: false,
-    lastUpdate: new Date()
-  },
-  {
-    roomId: "114",
-    isOn: true,
-    temperature: 25,
-    mode: mode.cold,
-    windSpeed: 2,
-    isWeeping: false,
-    lastUpdate: new Date()
-  }
-]);
+const roomIDs = ref<string[]>(["114", "514", "191", "115", "513", "192", "119"]);
 </script>
 
 <style scoped></style>
