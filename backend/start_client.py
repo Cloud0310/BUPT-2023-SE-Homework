c465e1890077ed1df6cbd95f9cfb458514134ec4
L.B <1059679598@qq.com>
2023-12-17 15:04:04 +0800

    modified:   backend/start_client.py

--- backend/start_client.py
+++ backend/start_client.py
@@ -1,15 +1,14 @@
 from flask.cli import FlaskGroup
 from app.scheduler import scheduler
-from app import create_app
 import asyncio
 
-bupt_se_app = create_app()
-bupt_vpl_app = create_app()
-bupt_se_client = FlaskGroup(bupt_se_app)
+from app import create_app
+
+app = create_app()
+cli = FlaskGroup(app)
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    # loop.run_until_complete(scheduler.run_scheduler())
-    # loop.run_forever()
     loop.create_task(scheduler.run_scheduler())
-    bupt_se_client()
+    cli()
