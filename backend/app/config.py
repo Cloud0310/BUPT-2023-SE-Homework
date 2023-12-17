90f6900cb88919d1529a8f6161795bcab05067a6
L.B <1059679598@qq.com>
2023-12-17 15:07:29 +0800

    modified:   backend/app/config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,22 +1,20 @@
+# config.py
 import os
 
-"""
-Hint: config.py is a file that contains all the configuration variables for the app.
-"""
-
-def get_env_variable(name):
-    try:
-        return os.environ[name]
-    except KeyError:
-        message = "Expected environment variable '{}' not set.".format(name)
-        raise Exception(message)
 
 class Config:
-    # Config server address and port
+    # 配置服务器地址和端口
+    # HOST = "localhost"
     FLASK_RUN_HOST = "0.0.0.0"
     FLASK_RUN_PORT = 11451
-    # Config database
+
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.abspath("data/Bupt.db")
-    SECRET_KEY = os.environ["SECRET_KEY"] if "SECRET_KEY" in os.environ else os.urandom(24)
+    SECRET_KEY = (
+        os.environ["SECRET_KEY"] if "SECRET_KEY" in os.environ else os.urandom(24)
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # # 配置服务器地址和端口
+    # HOST = "localhost"
+    # POST = 11451
