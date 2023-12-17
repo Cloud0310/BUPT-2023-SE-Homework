7f4d9401360080e28ceeadbf35205b4f05901596
L.B <1059679598@qq.com>
2023-12-17 15:09:42 +0800

    modified:   backend/app/utils.py

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,28 +1,30 @@
-from rsa import VerificationError
 import base64
+
 import rsa
-
-"""
-Hint: utils.py is a file that contains all the utility functions for the app.
-"""
-
-def generate_timestamp_id():
-    random_shuffled = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    return "".join(random_shuffled[i] for i in range(32))
+from rsa import VerificationError
 
 
-# Generate a timestamp id
-def verify_signature(verify_str, public_key, signature): 
-    public_key_parsed = rsa.PublicKey.load_pkcs1_openssl_pem(("-----BEGIN PUBLIC KEY-----\n" + public_key + "\n-----END PUBLIC KEY-----").encode())
+# 需要修改
+def check_csrf_token(requests):
+    csrf_token = requests.headers.get("X-CSRF-Token")
+
+    # 在这里进行比较
+    # return csrf_token == "your_expected_csrf_token"  # 这里只是示例
+    return 1
+
+
+def verify_signature(verify_str, public_key, signature):
+    public_key_parsed = rsa.PublicKey.load_pkcs1_openssl_pem(
+        (
+            "-----BEGIN PUBLIC KEY-----\n" + public_key + "\n-----END PUBLIC KEY-----"
+        ).encode()
+    )
     try:
-        rsa.verify(verify_str.encode(), base64.urlsafe_b64decode(signature.encode()), public_key_parsed)
+        rsa.verify(
+            verify_str.encode(),
+            base64.urlsafe_b64decode(signature.encode()),
+            public_key_parsed,
+        )
         return True
     except VerificationError:
         return False
-
-
-# Check if the csrf token is valid
-def check_csrf_token(requests):
-    csrf_token = requests.headers.get("X-CSRF-Token")
-    print("CSRF_TOKEN: ", csrf_token)
-    return 1
