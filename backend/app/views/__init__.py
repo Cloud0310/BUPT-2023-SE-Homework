c36d654128a6a430290b0503c168aaf59aa90df5
L.B <1059679598@qq.com>
2023-12-17 15:05:25 +0800

    modified:   backend/app/views/__init__.py

--- backend/app/views/__init__.py
+++ backend/app/views/__init__.py
@@ -1,9 +1,46 @@
-from flask import Blueprint, jsonify
+from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-hello_blueprint = Blueprint("hello", __name__)
-CORS(hello_blueprint)
+# 创建 SQLAlchemy 实例
+db = SQLAlchemy()
+migrate = Migrate()
 
-@hello_blueprint.route("/hello", methods=["GET"])
-def hello_world():
-    return jsonify(message="Hello, World!")
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app, origins="*", supports_credentials=True)
+
+    # 导入配置
+    app.config.from_object("app.config.Config")
+
+    # 修改 host 和 port
+    app.config["HOST"] = "0.0.0.0"
+    app.config["PORT"] = 11451
+
+    # 初始化扩展
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # 注册蓝图
+    from app.views.auth import auth_blueprint
+
+    # from app.views.data import data_blueprint
+    from app.views.query import query_blueprint
+    from app.views.room import room_blueprint
+    from app.views.admin import admin_blueprint
+    from app.views.control import control_blueprint
+    from app.views.client import client_blueprint
+
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(query_blueprint)
+    app.register_blueprint(room_blueprint)
+    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(control_blueprint)
+    app.register_blueprint(client_blueprint)
+
+    # 初始化房间
+
+    # app.run(host=host, port=port)
+    return app
