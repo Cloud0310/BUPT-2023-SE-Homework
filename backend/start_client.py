9fec67c8164f63b5dbb440acae52483e09e4aa63
L.B <1059679598@qq.com>
2023-12-17 15:15:56 +0800

    modified:   backend/start_client.py

--- backend/start_client.py
+++ backend/start_client.py
@@ -1,14 +1,21 @@
-from flask.cli import FlaskGroup
-from app.scheduler import scheduler
 import asyncio
-
+from flask.cli import FlaskGroup
 from app import create_app
+from app.scheduler import scheduler
 
+# 创建 Flask 应用程序
 app = create_app()
+
+# 创建 Flask CLI 组
 cli = FlaskGroup(app)
 
 
 if __name__ == "__main__":
+    # 获取事件循环
     loop = asyncio.get_event_loop()
-    loop.create_task(scheduler.run_scheduler())
+
+    # 创建任务并运行调度器
+    scheduler_task = loop.create_task(scheduler.run_scheduler())
+
+    # 启动 Flask CLI
     cli()
