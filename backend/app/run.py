2639c1e1cdf95c0b14e62ed3d92da2fb69ad6f1b
L.B <1059679598@qq.com>
2023-12-17 15:08:37 +0800

    modified:   backend/app/run.py

--- backend/app/run.py
+++ backend/app/run.py
@@ -1,12 +1,7 @@
-from flask_cors import CORS
 from app import create_app
 from app.scheduler import scheduler
-
-"""
-Hint: run.py is a file that is run when the app is started.
-"""
 
 if __name__ == "__main__":
     app = create_app()
     scheduler.run_scheduler(app.app_context())
-    app.run(host="0.0.0.0", port=11451)
+    app.run(host="0.0.0.0", port=11451, debug=False)
